b930632bea6feb2e8c2ad5a836ee7ce9e9aaaf2e
Clara <clarafdezfraga@gmail.com>
2024-03-21 23:43:41 +0100

    Change loginUser using ternary operator

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -6,23 +6,16 @@ import { askUserNewGiveawayData } from "./ui.js";
 import { types } from "node:util";
 
 export const loginUser = (email: string, password: string): void => {
-  if (
-    programData.users.some(
-      (user) => user.email === email && user.password === password
-    ) === false
-  ) {
-    console.log("lo sentimos los datos no son correctos"), process.exit(0);
-  } else {
-    programData.userEmail = email;
-    programData.isAdmin = programData.isAdmin;
-    console.log("¡Bienvenido!");
-    saveData();
-  }
+  programData.users.some(
+    (user) => user.email === email && user.password === password
+  )
+    ? ((programData.userEmail = email), console.log("¡Bienvenido!"), saveData())
+    : (console.log("Lo sentimos los datos no son correctos ¡BYE!"),
+      process.exit(0));
 };
 
 export const createGiveaway = (): void => {
-  const newGiveaway = askUserNewGiveawayData();
-  programData.giveaways.push(newGiveaway);
+  //...
   saveData();
   console.log("Se ha añadido un nuevo sorteo");
 };
