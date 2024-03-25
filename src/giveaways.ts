6043a4a3635495e2f0d7e7932588c00e8f483ff8
Clara <clarafdezfraga@gmail.com>
2024-03-25 11:14:48 +0100

    Fix wrong ternary expressions in createGiveaways & deleteGiveaway, add a message if user tries to sign up 2x in the same giveaway, change for loop for forEach() in listGiveaways.

--- src/giveaways.ts
+++ src/giveaways.ts
@@ -1,57 +1,93 @@
-import { PassThrough } from "node:stream";
-import { askUser } from "./askUser.js";
 import { programData, saveData } from "./storage.js";
 import { Giveaway, User } from "./types.js";
 import { askUserNewGiveawayData } from "./ui.js";
-import { types } from "node:util";
 
 export const loginUser = (email: string, password: string): void => {
-  const isUser = programData.users.find(
+  const user = programData.users.find(
     (user) => user.email === email && user.password === password
   );
-  isUser
-    ? ((programData.userEmail = email),
-      (programData.isAdmin = isUser.isAdmin),
-      console.log("¡Bienvenido!"),
-      saveData())
-    : (console.log("Lo sentimos los datos no son correctos ¡BYE!"),
-      process.exit(0));
+
+  if (user == undefined) {
+    console.log(
+      `
+Lo sentimos los datos no son correctos ¡BYE!`
+    );
+    process.exit(1);
+  } else {
+    programData.userEmail = email;
+    programData.isAdmin = user.isAdmin;
+    console.log(
+      `
+¡Bienvenido!`
+    );
+    saveData();
+  }
 };
 
 export const createGiveaway = (): void => {
   const newGiveaway = askUserNewGiveawayData();
-  programData.giveaways.push({
-    name: newGiveaway.giveawayName,
-    socialNetwork: newGiveaway.giveawaySocialNetwork,
-    participants: [],
-  });
-  saveData();
-  console.log("Se ha añadido un nuevo sorteo");
+
+  if (
+    programData.giveaways.some(
+      (giveaway) =>
+        giveaway.name === newGiveaway.giveawayName &&
+        giveaway.socialNetwork === newGiveaway.giveawaySocialNetwork
+    )
+  ) {
+    console.log(
+      `
+Ese sorteo ya existe`
+    );
+  } else {
+    programData.giveaways.push({
+      name: newGiveaway.giveawayName,
+      socialNetwork: newGiveaway.giveawaySocialNetwork,
+      participants: [],
+    });
+    saveData();
+    console.log(
+      `
+Se ha añadido un nuevo sorteo`
+    );
+  }
 };
 
 export const listGiveaways = (): void => {
   if (programData.giveaways.length === 0) {
-    console.log("En este momento no hay sorteos disponibles");
+    console.log(
+      `
+En este momento no hay sorteos disponibles.`
+    );
   } else {
-    console.log(`Éstos son los ${programData.giveaways.length} sorteos disponibles:
-    `);
-    for (let index = 0; index < programData.giveaways.length; index++) {
+    console.log(
+      `
+Éstos son los ${programData.giveaways.length} sorteos disponibles:
+      `
+    );
+
+    programData.giveaways.forEach((giveaway, index) =>
       console.log(
-        `${index + 1}. ${programData.giveaways.at(index)?.name} en ${
-          programData.giveaways.at(index)?.socialNetwork
-        }.`
-      );
-    }
+        `${index + 1}. ${giveaway.name} en ${giveaway.socialNetwork}.`
+      )
+    );
   }
 };
 
 export const deleteGiveaway = (giveawayNumber: number): void => {
   const selectedGiveaway = programData.giveaways[giveawayNumber - 1];
-  selectedGiveaway
-    ? programData.giveaways.splice(giveawayNumber - 1, 1) &&
-      console.log("Se ha borrado el sorteo")
-    : console.log("El sorteo no existe");
-  saveData();
+  if (selectedGiveaway) {
+    programData.giveaways.splice(giveawayNumber - 1, 1);
+    console.log(
+      `
+Se ha borrado el sorteo`
+    );
+  } else {
+    console.log(
+      `
+El sorteo que intentas borrar no existe`
+    );
+    saveData();
+  }
 };
 
 export const enterGiveaway = (giveawayNumber: number): void => {
@@ -60,20 +96,40 @@ export const enterGiveaway = (giveawayNumber: number): void => {
     (user) => user.email === programData.userEmail
   );
   if (!currentUser) {
-    console.log("Error:algo fallo");
-    return;
+    console.log(
+      `
+ups!lgo ha pasado con tu cuenta,inicia sesion por favor`
+    );
+    process.exit(1);
   }
 
-  selectedGiveaway
-    ? programData.giveaways.at(giveawayNumber - 1)?.participants.push({
-        name: currentUser?.name,
-        email: programData.userEmail,
-        password: currentUser?.password,
-        isAdmin: programData.isAdmin,
-      }) && console.log("Te has inscrito con éxito")
-    : console.log("No existe ese sorteo");
-
-  saveData();
+  if (
+    selectedGiveaway.participants.some(
+      (user) => user.email === currentUser.email
+    )
+  ) {
+    console.log(
+      `
+Ya te estabas inscrito en ese sorteo`
+    );
+  } else if (selectedGiveaway) {
+    programData.giveaways.at(giveawayNumber - 1)?.participants.push({
+      name: currentUser.name,
+      email: programData.userEmail,
+      password: currentUser.password,
+      isAdmin: programData.isAdmin,
+    });
+    console.log(
+      `
+Te has inscrito con éxito`
+    );
+    saveData();
+  } else {
+    console.log(
+      `
+No existe ese sorteo`
+    );
+  }
 };
 
 export const listUserGiveaways = (): void => {
@@ -83,8 +139,11 @@ export const listUserGiveaways = (): void => {
     )
   );
   if (userGiveaways.length >= 1) {
-    console.log(`Estás inscrito en los siguientes ${userGiveaways.length} torneos:
-  `);
+    console.log(
+      `
+Estás inscrito en los siguientes ${userGiveaways.length} torneos:
+  `
+    );
     userGiveaways.forEach((giveaway) =>
       console.log(
         `${userGiveaways.indexOf(giveaway) + 1}. ${giveaway.name} en ${
@@ -93,6 +152,9 @@ export const listUserGiveaways = (): void => {
       )
     );
   } else {
-    console.log("No estas inscrito en ningún sorteo");
+    console.log(
+      `
+No estas inscrito en ningún sorteo`
+    );
   }
 };
